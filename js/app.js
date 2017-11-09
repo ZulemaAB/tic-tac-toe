f466c43cda2177786fd9d525e812bb9be3053491
ZulemaAB <malezu06@hotmail.com>
2017-11-09 06:20:37 -0500

    ejercicio-tictacttoe-html,css,jd

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var container = document.querySelector('.tictactoe');
+
+
+container.addEventListener('click',onCellClick);
+
+function onCellClick(event) {
+  var target = event.target;
+  var dataset = target.datset;
+}
